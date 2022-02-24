eb801f31c138a57a660f84765c29642adb04bc50
Hartman55 <>
2022-02-24 03:52:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+#Create a program and use input() three times to get answers to the following questions from a user.  Store each of the answers in a variable.
+
+#What is your name?
+#What is your quest?
+#What is your favorite color?
+#Then, concatenate everything into a string within a print() statement with the form "So your name is [name], your quest is [quest], and your favorite color is [color]."
+
+var1 = input("what is your name? ")
+var2 = input("what is your quest? ")
+var3 = input("what is your favorite color? ")
+print("So your name is " + var1 + ", your quest is " +var2+ ", and your favorite color is " + var3)
